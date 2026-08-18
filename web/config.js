53c11ae1f393055a067838522bf84fc02126b601
mirrip <mirokearos@gmail.com>
2026-08-19 00:44:17 +0300

    Connect production Apps Script API

--- web/config.js
+++ web/config.js
@@ -1,4 +1,4 @@
 // Публичные настройки клиента. Секретов в этом файле быть не должно.
-globalThis.SHEET_MESSENGER_API_URL = "";
+globalThis.SHEET_MESSENGER_API_URL = "https://script.google.com/macros/s/AKfycbzHiuPLpLvjnwTJSfvucSI3V1E2Ay9kPfqprqHY_qLU3btkVjfBxNmExdETvQ4PtTIVLg/exec";
 globalThis.SHEET_MESSENGER_FIREBASE_CONFIG = null;
 globalThis.SHEET_MESSENGER_FIREBASE_VAPID_KEY = "";
