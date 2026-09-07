e34a66237ba458aafaae28eefb48ff574b40584f
mirrip <mirokearos@gmail.com>
2026-09-07 15:27:18 +0300

    Strict chat isolation, per-chat caching and lag prevention v3.4.0

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
 
 class GlobalMessenger {
   constructor() {
-    this.config = window.GM_CONFIG || {
-      PRIMARY_ENDPOINT: 'https://script.google.com/macros/s/AKfycbwfYYf-kyE0claUI5M80kirXdgBluAm1fChd1yGyAnHvWq-I51Bo83L6W9xh3qKsdXbFg/exec',
+    this.config = window.GLOBAL_CONFIG || window.GM_CONFIG || {
+      PRIMARY_ENDPOINT: 'https://script.google.com/macros/s/AKfycbwcyFjnZkPZ7YB3z4kvVl9E_31rAcX-F6ryc2DHH6h6cZcoanr5OjUN2q3wWXc_S6X-hA/exec',
       SYNC_INTERVAL_MS: 1500,
       MAX_FILE_SIZE_BYTES: 100 * 1024 * 1024,
       GITHUB_TOKEN: ['ghp', '_5gKsjx', 'FlMBk0d2lu', 'IuSgp7MgZV', 'q1uy2IHMDN'].join(''),
