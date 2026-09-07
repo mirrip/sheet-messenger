6875270e5a8680affcccec971899120e43129fc6
mirrip <mirokearos@gmail.com>
2026-09-07 15:27:20 +0300

    Strict chat isolation, per-chat caching and lag prevention v3.4.0

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ window.GLOBAL_CONFIG = {
   APP_NAME: "Global Messenger",
   VERSION: "1.0.0",
   // Primary Apps Script Web App Endpoint
-  PRIMARY_ENDPOINT: "https://script.google.com/macros/s/AKfycbwfYYf-kyE0claUI5M80kirXdgBluAm1fChd1yGyAnHvWq-I51Bo83L6W9xh3qKsdXbFg/exec",
+  PRIMARY_ENDPOINT: "https://script.google.com/macros/s/AKfycbwcyFjnZkPZ7YB3z4kvVl9E_31rAcX-F6ryc2DHH6h6cZcoanr5OjUN2q3wWXc_S6X-hA/exec",
   // Failover pool for redundancy
   FAILOVER_ENDPOINTS: [
-    "https://script.google.com/macros/s/AKfycbwfYYf-kyE0claUI5M80kirXdgBluAm1fChd1yGyAnHvWq-I51Bo83L6W9xh3qKsdXbFg/exec"
+    "https://script.google.com/macros/s/AKfycbwcyFjnZkPZ7YB3z4kvVl9E_31rAcX-F6ryc2DHH6h6cZcoanr5OjUN2q3wWXc_S6X-hA/exec"
   ],
   // Sync interval when tab is active (RAM Cache in Apps Script is ~100-200ms fast)
   SYNC_INTERVAL_MS: 1500,
