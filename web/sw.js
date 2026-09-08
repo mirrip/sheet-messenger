9ad2bba35ea4d374e8e88518b403e59ae89458f9
mirrip <mirokearos@gmail.com>
2026-09-08 15:31:46 +0300

    v3.8.0: fix mobile recording kick-out, safe history popstate, reliable circle playback

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
 // Clear cache
-self.addEventListener(" install\, () => self.skipWaiting());
+self.addEventListener(\install\, () => self.skipWaiting());
 self.addEventListener(\activate\, (event) => { event.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k)))).then(() => self.registration.unregister())); });
 self.addEventListener(\fetch\, (event) => { event.respondWith(fetch(event.request)); });
