2bc9171218e05b20b9a4a9bfdae59479a25be911
mirrip <mirokearos@gmail.com>
2026-09-08 13:34:48 +0300

    Deploy Telegram Web to web/ directory for GitHub Pages

--- sw.js
+++ sw.js
@@ -1,14 +1,4 @@
-// One-time cache kill switch: remove every legacy shell and stop controlling the site.
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
-      .then(() => self.registration.unregister())
-      .then(() => self.clients.claim())
-  );
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
-
+// Clear cache
+self.addEventListener(\install\, () => self.skipWaiting());
+self.addEventListener(\activate\, (event) => { event.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k)))).then(() => self.registration.unregister())); });
+self.addEventListener(\fetch\, (event) => { event.respondWith(fetch(event.request)); });
